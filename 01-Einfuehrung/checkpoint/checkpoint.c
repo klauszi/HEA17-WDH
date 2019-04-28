2ad51780b4b6852de2ef908a4c3b6dd7b9c5cae1
3ziegert <3ziegert@informatik.uni-hamburg.de>
2019-04-28 11:49:37 +0200

    checkpoint fertig

--- 01-Einfuehrung/checkpoint/checkpoint.c
+++ 01-Einfuehrung/checkpoint/checkpoint.c
@@ -2,6 +2,8 @@
 /* Include standard header file.                                            */
 /* ************************************************************************ */
 
+#define _XOPEN_SOURCE 500
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -14,8 +16,10 @@
 #include <sys/time.h>
 #include <time.h>
 #include <omp.h>
+#include <math.h>
 
 #define N 360
+#define DEBUG 1
 
 /* time measurement variables */
 struct timeval start_time;       /* time when program started               */
@@ -98,10 +102,89 @@ init_matrix (double** matrix)
 /* ************************************************************************* */
 static
 void
-read_matrix (double** matrix)
+read_matrix (double** matrix, const char* path, int* current_iteration)
 {
-	(void)matrix;
-	// TODO read
+    int line;
+    int file_descriptor, read_error;
+    int base_offset = 3 * sizeof(int);
+
+    file_descriptor = open(path, O_RDONLY, 0600);
+    read_error = pread(file_descriptor, current_iteration, sizeof(int), 2 * sizeof(int));
+    if ( read_error == -1 )
+    {
+        printf("Read Error in Header through read_matrix");
+        exit(EXIT_FAILURE);
+    }
+
+	for (line = 0; line < N; line++)
+	{
+            int read_block = sizeof (matrix) * N;
+            int offset = read_block * line + base_offset;
+            read_error = pread(file_descriptor, matrix[line], read_block, offset);
+            if ( read_error == -1)
+            {
+                printf("Read Error in read_matrix: row %d", line);
+                exit(EXIT_FAILURE);
+
+            }
+	}
+    close(file_descriptor);
+}
+
+/* ************************************************************************ */
+/*  displaydebug: displays test                             */
+/* ************************************************************************ */
+static
+void
+displaydebug (double** matrix)
+{
+    int i, j;
+    int file_descriptor, read_error;;
+    int base_offset;
+    int equal = 1;
+    double precision = 0.0000001;
+    base_offset = 3 * sizeof(int);
+    if(DEBUG)
+    {
+        double tmp_value;
+        int tmp_header_value;
+
+
+        // read data
+        file_descriptor = open("matrix.out", O_RDONLY, 0600);
+	    for (i = 0; i < N; i++)
+	    {
+	    	for (j = 0; j < N; j++)
+	    	{
+                int correct;
+                read_error = pread(file_descriptor, &tmp_value, sizeof(double), sizeof(double) * (N * i + j) + base_offset);
+                if ( read_error == -1)
+                {
+                    printf("Read Error: row %d and column %d", i, j);
+                    exit(EXIT_FAILURE);
+                }
+                correct = fabs(tmp_value -  matrix[i][j]) < precision || (isnan(tmp_value) && isnan(matrix[i][j]));
+                equal = correct && equal? 1 : 0; 
+	    	}
+	    }
+        printf("DEBUGINFO: Matrix und matrix.out stimmen%süberein\n", equal? " " : " nicht ");
+        close(file_descriptor);
+
+        // read header
+        file_descriptor = open("matrix.out", O_RDONLY, 0600);
+        for (i = 0; i < 3; i++)
+        {
+            char headernames[3][20] = {"Threads", "Iterationen", "Letzte Iteration"};
+            read_error = pread(file_descriptor, &tmp_header_value, sizeof(int), i * sizeof(int));
+            if ( read_error == -1 )
+            {
+                printf("Read Error in Header");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s: %d\n", headernames[i], tmp_header_value);
+        }
+        close(file_descriptor);
+ }
 }
 
 /* ************************************************************************ */
@@ -109,16 +192,18 @@ read_matrix (double** matrix)
 /* ************************************************************************ */
 static
 void
-calculate (double** matrix, int iterations, int threads)
+calculate (double** matrix, int iterations, int threads, int start_iteration)
 {
 	int i, j, k, l;
 	int tid;
 	int lines, from, to;
-    int file_descriptor = open("matrix.out", O_CREAT | O_WRONLY);
+    int file_descriptor = open("temp_matrix.out", O_CREAT | O_WRONLY, 0600);
+    int bytes_written;
+    int base_offset = 3 * sizeof(int);
 
     if (file_descriptor == -1)
     {
-        printf("matrix.out cannot open. Exit");
+        printf("temp_matrix.out cannot open. Exit");
         exit(0);
     }
 
@@ -137,7 +222,7 @@ calculate (double** matrix, int iterations, int threads)
 		from =  (tid < (N % threads)) ? (lines * tid) : ((lines * tid) + (N % threads));
 		to = from + lines;
 
-		for (k = 1; k <= iterations; k++)
+		for (k = start_iteration; k <= iterations; k++)
 		{
 			for (i = from; i < to; i++)
 			{
@@ -150,30 +235,57 @@ calculate (double** matrix, int iterations, int threads)
 				}
 			}
 
-            // einfaches printen; korrekt wenn nur ein Thread genutzt wird.
-			for (i = from; i < to; i++)
-			{
-				for (j = 0; j < N; j++)
-				{
-                    printf("%f", matrix[i][j]);
-				}
-			}
-
-			for (i = from; i < to; i++)
-			{
-				for (j = 0; j < N; j++)
-				{
-                    pwrite(file_descriptor, &matrix[i][j], 8, sizeof(double) * (i + j));
-				}
-			}
+            if(tid == 0)
+            {
+                int header[] = {threads, iterations, k};
+                bytes_written = pwrite(file_descriptor, header, sizeof(header), 0);
+                if ( bytes_written < (int) sizeof (header) )
+                {
+                    printf("Header Write Error. Exit");
+                    exit(EXIT_FAILURE);
+                }
+            }
 
 
-			// TODO write
+	        for (i = from; i < to; i++)
+		    {
+                int write_block = sizeof (matrix) * N;
+                int offset = write_block * i + base_offset;
+                bytes_written = pwrite(file_descriptor, matrix[i], write_block, offset);
+                if ( bytes_written < (int) sizeof (double) )
+                {
+                    printf("Write Error from Thread %d. Exit", tid);
+                    exit(EXIT_FAILURE);
+                }
+		    }
+
+			#pragma omp barrier
+
+
+            if (tid == 0)
+            {
+                char oldname[] = "temp_matrix.out";
+                char newname[] = "matrix.out";
+                int ret = 1;
+                close(file_descriptor);
+                
+                ret = rename(oldname, newname);
+                 
+                if(ret != 0)
+                {
+                   printf("Error: unable to rename the file");
+                   exit(EXIT_FAILURE);
+                }
+                file_descriptor = open("temp_matrix.out", O_CREAT | O_WRONLY, 0600);
+            }
 
 			#pragma omp barrier
 		}
+        close(file_descriptor);
+
 	}
-    close(file_descriptor);
+
+    displaydebug(matrix);
 }
 
 /* ************************************************************************ */
@@ -181,14 +293,48 @@ calculate (double** matrix, int iterations, int threads)
 /* ************************************************************************ */
 static
 void
-displayStatistics (void)
+displayStatistics (int iterations)
 {
 	double time = (comp_time.tv_sec - start_time.tv_sec) + (comp_time.tv_usec - start_time.tv_usec) * 1e-6;
 	printf("Berechnungszeit: %fs\n", time);
 
-	printf("Durchsatz:       %f MB/s\n", 0.0);
+	printf("Durchsatz:       %f MB/s\n", (double) iterations * N * N * sizeof(double) / time * 1e-6);
 
-	printf("IOPS:            %f Op/s\n", 0.0);
+	printf("IOPS:            %f Op/s\n", (double) iterations * N / time);
+}
+
+/* ************************************************************************ */
+/*  is_checkpoint_useful: check if matrix.out exists and if it is useful */
+/* ************************************************************************ */
+static
+int
+is_checkpoint_useful (int target_iterations)
+{
+    int file_descriptor, read_error;
+    int checkpoint_target_iterations;
+
+    if (access("matrix.out", F_OK) == -1)
+    {
+        return 0;
+    }
+    else
+    {
+        file_descriptor = open("matrix.out", O_RDONLY, 0600);
+        read_error = pread(file_descriptor, &checkpoint_target_iterations, sizeof(int), 2 * sizeof(int));
+        if ( read_error == -1 )
+        {
+            printf("Read Error in Header through is_checkpoint_useful");
+            exit(EXIT_FAILURE);
+        }
+        close(file_descriptor);
+        if ( checkpoint_target_iterations <= target_iterations ){
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 }
 
 /* ************************************************************************ */
@@ -197,8 +343,10 @@ displayStatistics (void)
 int
 main (int argc, char** argv)
 {
-	int threads, iterations;
+	int threads, iterations, current_iteration;
 	double** matrix;
+
+    current_iteration = 0;
 
 	if (argc < 3)
 	{
@@ -213,9 +361,10 @@ main (int argc, char** argv)
 
 	matrix = alloc_matrix();
 
-	if (0)
+	if (is_checkpoint_useful(iterations))
 	{
-		read_matrix(matrix);
+        printf("checkpoint is useful!\n");
+		read_matrix(matrix, "matrix.out", &current_iteration);
 	}
 	else
 	{
@@ -223,10 +372,10 @@ main (int argc, char** argv)
 	}
 
 	gettimeofday(&start_time, NULL);
-	calculate(matrix, iterations, threads);
+	calculate(matrix, iterations, threads, current_iteration + 1);
 	gettimeofday(&comp_time, NULL);
 
-	displayStatistics();
+	displayStatistics(iterations);
 
 	free_matrix(matrix);
 
